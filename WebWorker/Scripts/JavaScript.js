a05e33e21421cc638a00e63afc9e3a777287d56e
Martin Orczykowski <Martin.Orczykowski@drheuer.de>
2023-05-08 12:07:18 +0200

    Projektdateien hinzufügen.

---
+++ WebWorker/Scripts/JavaScript.js
@@ -0,0 +1,36 @@
+﻿let worker;
+
+$("#start").on("click", function () {
+    worker = new Worker("Scripts/task.js");
+
+    $("#start").attr("disabled", true);
+
+    worker.onmessage = function (event) {
+        let o = JSON.parse(event.data);
+
+        if (o.Typ == "result") {
+            $("#pi").val(o.Wert);
+            $("#start").attr("disabled", false);
+        }
+        else {
+            $("#progress").val(o.Wert);
+        }
+    };
+});
+
+$("#stop").on("click", function () {
+    worker.terminate();
+});
+
+class Nachricht {
+    constructor(typ, wert) {
+        this.Typ = typ;
+        this.Wert = wert;
+    }
+}
+
+
+
+
+
+
---
+++ WebWorker/Scripts/task.js
@@ -0,0 +1,28 @@
+﻿self.onmessage = function (event) {
+
+};
+
+let text = { "Typ": "result", "Wert": computePi() };
+
+let s = JSON.stringify(text);
+
+self.postMessage(s);
+
+function computePi() {
+    let sum = 0.0;
+    const step = 1e-9;
+    for (let i = 0; i < 1_000_000_000; i++) {
+        let x = (i + 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
+
+        if (i % 1000000 == 0) {
+            let text2 = { "Typ": "counter", "Wert": i };
+
+            let o = JSON.stringify(text2);
+
+            self.postMessage(o);
+        }
+
+    }
+    return sum * step;
+}
